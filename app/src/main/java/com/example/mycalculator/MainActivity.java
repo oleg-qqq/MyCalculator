e3ad06641ba8f092e4bbf46f83731ed9f5b2488b
Oleg <2020610@mail.ru>
2022-05-04 15:09:47 +0400

    Добавлена обработка кнопок с помощью оператора СВИТЧ.

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+
+    }
+
+    public void clickNumber(View view) {
+        String number = editText.getText().toString();
+        switch (view.getId()){
+            case R.id.button1: number = number+"1" ; break;
+            case R.id.button2: number = number+"2" ; break;
+            case R.id.button3: number = number+"3" ; break;
+            case R.id.button4: number = number+"4" ; break;
+            case R.id.button5: number = number+"5" ; break;
+            case R.id.button6: number = number+"6" ; break;
+            case R.id.button7: number = number+"7" ; break;
+            case R.id.button8: number = number+"8" ; break;
+            case R.id.button9: number = number+"9" ; break;
+            case R.id.button0: number = number+"0" ; break;
+            case R.id.buttonDot: number = number+"." ; break;
+        }
+        editText.setText(number);
     }
 }
