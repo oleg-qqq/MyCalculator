bf5ca8678225ec8b79e60da91764ded260ba8b96
Oleg <2020610@mail.ru>
2022-05-13 19:41:32 +0400

    Добавлены радиокнопки и реализована возможность переключать темы с дневной на ночную. Пересобран экран горизонтальной ориентации.

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.mycalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.google.android.material.radiobutton.MaterialRadioButton;
 
 public class MainActivity extends AppCompatActivity {
 //Объявляем переменные
@@ -15,10 +19,23 @@ public class MainActivity extends AppCompatActivity {
     Boolean isDot = true;
     Boolean isPlusMinus = true;
 
+    private static final String NameSharedPreference = "LOGIN";
+
+    private static final String DayTheme = "APP_THEME";
+    private static final int DayThemeCodeStyle = 0;
+    private static final int NightThemeCodeStyle = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setTheme(getAppTheme(R.style.DayTheme));
+
+
+
         setContentView(R.layout.activity_main);
+
+        initThemeChooser();
 //Получаем ID вью editText
         editText = findViewById(R.id.editText);
 
@@ -102,4 +119,62 @@ public class MainActivity extends AppCompatActivity {
 
         editText.setText(number);
     }
+    // Инициализация радиокнопок
+    private void initThemeChooser(){
+        initRadioButton(findViewById(R.id.radioButtonDay),
+                DayThemeCodeStyle);
+        initRadioButton(findViewById(R.id.radioButtonNight),
+                NightThemeCodeStyle);
+        RadioGroup rg = findViewById(R.id.radioButtons);
+        ((MaterialRadioButton)rg.getChildAt(getCodeStyle(DayThemeCodeStyle))).setChecked(true);
+    }
+// Все инициализации кнопок очень похожи, поэтому создадим метод для переиспользования
+private void initRadioButton(View button, final int codeStyle){
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+// сохраним настройки
+            setAppTheme(codeStyle);
+// пересоздадим активити, чтобы тема применилась
+            recreate();
+        }
+    });
 }
+
+    private int getAppTheme(int codeStyle) {
+        return codeStyleToStyleId(getCodeStyle(codeStyle));
+    }
+// Чтение настроек, параметр «тема»
+private int getCodeStyle(int codeStyle){
+// Работаем через специальный класс сохранения и чтения настроек
+    SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,
+            MODE_PRIVATE);
+//Прочитать тему, если настройка не найдена - взять по умолчанию
+    return sharedPref.getInt(DayTheme, codeStyle);
+}
+// Сохранение настроек
+private void setAppTheme(int codeStyle) {
+    SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,
+            MODE_PRIVATE);
+// Настройки сохраняются посредством специального класса editor.
+    SharedPreferences.Editor editor = sharedPref.edit();
+    editor.putInt(DayTheme, codeStyle);
+    editor.apply();
+}
+    private int codeStyleToStyleId(int codeStyle){
+        switch(codeStyle){
+            case DayThemeCodeStyle:
+                return R.style.DayTheme;
+            case NightThemeCodeStyle:
+                return R.style.NightTheme;
+            default:
+                return R.style.DayTheme;
+        }
+    }
+
+
+
+
+
+
+        }
